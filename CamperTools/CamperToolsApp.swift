4e8b212162b1b1c73b2e969b25dac1746766f319
becste <stephan.becker@gmail.com>
2026-01-31 21:12:36 -0700

    Initial Commit

---
+++ CamperTools/CamperToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CamperToolsApp.swift
+//  CamperTools
+//
+//  Created by Stephan Becker on 1/31/26.
+//
+
+import SwiftUI
+
+@main
+struct CamperToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
